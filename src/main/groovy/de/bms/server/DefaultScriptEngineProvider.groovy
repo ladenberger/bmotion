dc0e5e81a6405f766686548b6aac5d852eed19c1
Lukas Ladenberger <lukas.ladenberger@googlemail.com>
2014-11-14 12:28:24 +0100

    added default script engine provider

---
+++ src/main/groovy/de/bms/server/DefaultScriptEngineProvider.groovy
@@ -0,0 +1,14 @@
+package de.bms.server
+
+public class DefaultScriptEngineProvider implements BMotionScriptEngineProvider {
+
+    @Override
+    public GroovyShell get() {
+        return new GroovyShell()
+    }
+
+    @Override
+    public String[] getImports() {
+        return []
+    }
+}
