29e70836951198bacdfce5ad00ded87a168470a0
Lukas Ladenberger <lukas.ladenberger@googlemail.com>
2014-12-01 14:42:17 +0100

    changed api names

--- src/main/resources/content/libs/bmotion/bmotion.js
+++ src/main/resources/content/libs/bmotion/bmotion.js
@@ -221,7 +221,7 @@ define(["css!jquery-ui-css", "css!jquery-ui-theme-css", "css!bootstrap-css", "cs
     return {
         socket: socket,
         call: function(name, options) {
-            callFunction('callGroovyMethod', name, options)
+            callFunction('callMethod', name, options)
         },
         executeEvent: function(name, options) {
             callFunction('executeEvent', name, options)
