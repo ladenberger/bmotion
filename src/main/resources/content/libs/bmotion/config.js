c82a22ac7fd8bd808707906d9b5ddfef7214dcff
Lukas Ladenberger <lukas.ladenberger@googlemail.com>
2015-02-04 15:49:34 +0100

    shifted js libraries

--- src/main/resources/content/libs/bmotion/config.js
+++ src/main/resources/content/libs/bmotion/config.js
@@ -12,27 +12,18 @@ require.config({
         "ngBMotion": "/bms/libs/bmotion/ngBMotion",
         "jquery": "/bms/libs/jquery/jquery-1.11.0.min",
         "socketio": "/bms/libs/socket.io/socket.io",
-        "jquery-ui": "/bms/libs/jquery-ui/jquery-ui.min",
-        "jquery-cookie": "/bms/libs/jquery-cookie/jquery.cookie",
         "bootstrap": "/bms/libs/bootstrap/js/bootstrap.min",
         "bootstrap-css": "/bms/libs/bootstrap/css/bootstrap.min",
-        "jquery-ui-css": "/bms/libs/jquery-ui/jquery-ui.min",
-        "jquery-ui-theme-css": "/bms/libs/jquery-ui/jquery-ui.theme.min",
-        "tooltipster": "/bms/libs/tooltipster/jquery.tooltipster.min",
-        "tooltipster-css": "/bms/libs/tooltipster/tooltipster",
-        "tooltipster-shadow-css": "/bms/libs/tooltipster/themes/tooltipster-shadow",
         "bmotion-css": "/bms/libs/bmotion/bmotion"
     },
     shim: {
         'jquery': {'exports': '$'},
-        'jquery-ui': ['jquery'],
         'angular': {'exports': 'angular'},
         'angularAMD': ["angular"],
         'angular-route': ["angular"],
         'socketio': {'exports': 'io'},
         'ngBMotion': ["bms", "socketio", "bootstrap", "jquery-cookie"],
         'bootstrap': ["jquery"],
-        'tooltipster': ["jquery"],
         'bms': {'exports': 'bms'}
     }
 });
