53498329e34deb2e26595e537d321ee3228f3bd4
Lukas Ladenberger <lukas.ladenberger@googlemail.com>
2015-02-20 16:53:23 +0100

    disable open browser

--- src/main/groovy/de/bms/server/BMotionServer.groovy
+++ src/main/groovy/de/bms/server/BMotionServer.groovy
@@ -93,7 +93,6 @@ public class BMotionServer {
     public void start() {
         startBMotionSocketServer()
         startBMotionJettyServer()
-        openBrowser()
     }
 
     private startBMotionJettyServer() {
