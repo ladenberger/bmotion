dc7989b85cc3603e4d076397f7ada7c531e4286a
Lukas Ladenberger <lukas.ladenberger@googlemail.com>
2015-09-07 14:06:02 +0200

    append log to file

--- src/main/resources/logback.groovy
+++ src/main/resources/logback.groovy
@@ -1,10 +1,11 @@
 import ch.qos.logback.classic.encoder.PatternLayoutEncoder
 import ch.qos.logback.core.ConsoleAppender
 
-import static ch.qos.logback.classic.Level.ERROR
-import static ch.qos.logback.classic.Level.INFO
-import static ch.qos.logback.classic.Level.WARN
-import static ch.qos.logback.classic.Level.OFF
+import static ch.qos.logback.classic.Level.*
+
+// define the USER_HOME variable setting its value
+// to that of the "user.home" system property
+def USER_HOME = System.getProperty("user.home")
 
 appender("STDOUT", ConsoleAppender) {
     encoder(PatternLayoutEncoder) {
@@ -12,17 +13,17 @@ appender("STDOUT", ConsoleAppender) {
     }
 }
 
-/*appender("FILE", FileAppender) {
-    file = "/home/lukas/tmp/bmsserver.log"
+appender("FILE", FileAppender) {
+    file = "${USER_HOME}/.prob/bmotion.log"
     append = true
     encoder(PatternLayoutEncoder) {
         pattern = "%d{HH:mm:ss.SSS} %.-5level %logger{36} - %msg%n"
     }
-}*/
+}
 
 logger("de.prob.cli", OFF)
 logger("de.prob.animator", OFF)
 logger("org.eclipse.jetty", ERROR)
 logger("com.corundumstudio.socketio", WARN)
 //root(INFO, ["STDOUT", "FILE"])
-root(INFO, ["STDOUT"])
+root(INFO, ["STDOUT", "FILE"])
