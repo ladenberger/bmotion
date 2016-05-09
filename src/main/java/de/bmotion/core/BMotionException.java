1f4372ac7880de7be0e2db22a5fe6d662ff89c7f
Lukas Ladenberger <lukas.ladenberger@googlemail.com>
2016-05-09 14:29:23 +0200

    changed name

--- src/main/java/de/bmotion/core/BMotionException.java
+++ src/main/java/de/bmotion/core/BMotionException.java
@@ -5,7 +5,7 @@ public class BMotionException extends Exception {
 	private static final long serialVersionUID = 1L;
 
 	public BMotionException(String msg) {
-		super("BMotion Studio: " + msg);
+		super("BMotionWeb: " + msg);
 	}
 
 }
